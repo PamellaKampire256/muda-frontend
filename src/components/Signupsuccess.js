cff63cb3c739e93a2c76bf1e438642dd3ab5757c
Kampire <pamellakampire@gmail.com>
2023-09-07 10:04:40 +0300

    Changing the logo for sigin up success

--- src/components/Signupsuccess.js
+++ src/components/Signupsuccess.js
@@ -7,13 +7,13 @@ function Signupsuccess() {
   return (
     <div>
         <Navbar />
-<div class="page-ath-wrap" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+<div class="page-ath-wrap" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '70vh' }}>
         <div class="page-ath-content">
             <div class="page-ath-header">
-                <a href="/#/signupsuccess" class="page-ath-logo"><img src="assets/images/logo.png" srcset="assets/images/logo2x.png 2x" alt="logo" /></a>
+                <a href="/#/signupsuccess" class="page-ath-logo"><img src="assets/images/avatar8.png" srcset="assets/images/avatar9.png 2x" alt="logo" style={{ width: '250px', height: 'auto' }} /></a>
             </div>
             <div class="page-ath-text">
-                <h2 class="page-ath-heading">Thank you! <small>Your singup process is alomost done.</small> <span class="text-success">Please go to our next step and fill your personal details.</span></h2><Link to="/profile"><strong style={{ textDecoration: 'underline', fontSize: '1.4em' }}>Here</strong></Link>
+                <h2 class="page-ath-heading">Thank you! <small>Your singup process is alomost done.</small> <span class="text-success">Please go to our next step and fill in your personal details.</span></h2><Link to="/profile"><strong style={{ textDecoration: 'underline', fontSize: '1.4em' }}>Here</strong></Link>
             </div>
             <div class="page-ath-footer">
                
