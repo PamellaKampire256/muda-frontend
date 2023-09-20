f7bec394190c802eaf7ace1bde082463a5442a7f
Kampire <pamellakampire@gmail.com>
2023-09-21 00:07:47 +0300

    Integrating login and register with backend

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,89 +1,91 @@
-import React, {useState} from 'react';
-import { Routes, useNavigate, Route, Link } from 'react-router-dom';
-import Axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
+function Register() {
+  const navigate = useNavigate();
 
-// function Register() {
-//     const navigate= useNavigate();
-//   const handleRegister = () =>{
-//     navigate('/signupsuccess') 
-//   }
-  const Register = () =>{
-    const [userName, setUserName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [repeat_password, setRepeatPassword] = useState('')
+  const handleRegister = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-    const createUser = ()=>{
-        Axios.post('http://localhost:3001/auth/register', {
-            FullName: userName,
-            Email: email,
-            Password: password,
-            RepeatPassword: repeat_password
-        }).then(()=>{
-            console.log('User has been created')
-        })
-        .catch((error) => {
-            console.error('Error creating user:', error);
-          });
+    const form = event.target; // Get the form element
 
+    const newUser = {
+      full_name: form.elements.name.value,
+      email: form.elements.email.value,
+      password: form.elements.password.value,
+      repeat_password: form.elements.repeat_password.value,
+    };
+
+    try {
+      const response = await fetch('http://localhost:3002/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      if (response.ok) {
+        console.log('User has been created');
+        // You can redirect to a success page or perform other actions here.
+        navigate('/signupsuccess');
+      } else {
+        const errorData = await response.json(); // Parse the error response as JSON
+        console.error('Error creating user:', errorData.error);
+        // Handle the error and possibly display an error message to the user.
+      }
+    } catch (error) {
+      console.error('Error creating user:', error);
+      // Handle the error and possibly display an error message to the user.
     }
-  
+  };
+
   return (
     <div>
-   <div class="page-ath-wrap" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-        <div class="page-ath-content">
-            <div class="page-ath-header">
-                <a href="/#/register" class="page-ath-logo"><img src="assets/images/avatar8.png" srcset="assets/images/avatar9.png 2x" alt="logo" style={{ width: '200px', height: 'auto' }} /></a>
+      <div className="page-ath-wrap" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+        <div className="page-ath-content">
+          <div className="page-ath-header">
+            <a href="/#/register" className="page-ath-logo">
+              <img src="assets/images/avatar8.png" srcSet="assets/images/avatar9.png 2x" alt="logo" style={{ width: '200px', height: 'auto' }} />
+            </a>
+          </div>
+          <div className="page-ath-form">
+            <h2 className="page-ath-heading">Sign up <small>Create New MUDA-KYC Account</small></h2>
+            <form onSubmit={handleRegister}>
+              <div className="input-item">
+                <input type="text" name="name" placeholder="Your Name" className="input-bordered" />
+              </div>
+              <div className="input-item">
+                <input type="text" name="email" placeholder="Your Email" className="input-bordered" />
+              </div>
+              <div className="input-item">
+                <input type="password" name="password" placeholder="Password" className="input-bordered" />
+              </div>
+              <div className="input-item">
+                <input type="password" name="repeat_password" placeholder="Repeat_Password" className="input-bordered" />
+              </div>
+              <div className="input-item text-left">
+                <input className="input-checkbox input-checkbox-md" id="term-condition" type="checkbox" />
+                <label htmlFor="term-condition">I agree to MUDA KYC’s <a href="/#/policy">Privacy Policy</a> &amp;</label>
+              </div>
+              <button type="submit" className="btn btn-primary btn-block">Create Account</button>
+            </form>
+            <ul className="row guttar-20px guttar-vr-20px">
+            </ul>
+            <div className="gaps-2x"></div>
+            <div className="gaps-2x"></div>
+            <div className="form-note">
+              Already have an account? <Link to="/"> <strong>Sign in instead</strong></Link>
             </div>
-            <div class="page-ath-form">
-                <h2 class="page-ath-heading">Sign up <small>Create New MUDA-KYC Account</small></h2>
-                <form action="#">
-                    <div class="input-item">
-                        <input type="text" placeholder="Your Name" class="input-bordered" onChange={(event)=>{
-                            setUserName(event.target.value)
-                        }} />
-                    </div>
-                    <div class="input-item">
-                        <input type="text" placeholder="Your Email" class="input-bordered" onChange={(event)=>{
-                            setEmail(event.target.value)
-                        }} />
-                    </div>
-                    <div class="input-item">
-                        <input type="password" placeholder="Password" class="input-bordered" onChange={(event)=>{
-                            setPassword(event.target.value)
-                        }} />
-                    </div>
-                    <div class="input-item">
-                        <input type="password" placeholder="Repeat_Password" class="input-bordered" onChange={(event)=>{
-                            setRepeatPassword(event.target.value)
-                        }} />
-                    </div>
-                    <div class="input-item text-left">
-                        <input class="input-checkbox input-checkbox-md" id="term-condition" type="checkbox" />
-                        <label for="term-condition">I agree to MUDA KYC’s <a href="/#/policy">Privacy Policy</a> &amp;</label>
-                    </div>
-                    <button class="btn btn-primary btn-block" onClick={createUser}>Create Account</button>
-                </form>
-                <ul class="row guttar-20px guttar-vr-20px">  
-                </ul>
-                <div class="gaps-2x"></div>
-                <div class="gaps-2x"></div>
-                <div class="form-note">
-                    Already have an account ? <Link to="/"> <strong>Sign in instead</strong></Link>
-                </div>
-            </div>
-            <div class="page-ath-footer">
-                <ul class="footer-links">
-                   
-                </ul>
-            </div>
+          </div>
+          <div className="page-ath-footer">
+            <ul className="footer-links">
+            </ul>
+          </div>
         </div>
-      
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-// }
 
-export default Register
+export default Register;
