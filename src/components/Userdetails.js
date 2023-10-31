37ff30725982805e9eb3b07ddee7bbfa3503e2f6
Kampire <pamellakampire@gmail.com>
2023-11-01 01:54:22 +0300

    api integration for users

--- src/components/Userdetails.js
+++ src/components/Userdetails.js
@@ -1,112 +1,142 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Navbar from './Navbar';
 
-function Userdetails() {
+function UserDetails() {
+  const { userId } = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:3002/all_users/user/${userId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data.user_information);
+      })
+      .catch((error) => {
+        console.error('Error fetching user details: ', error);
+      });
+  }, [userId]);
+
   return (
     <div>
-        <Navbar />
-    <div class="page-content">
-        <div class="container">
-            <div class="card content-area">
-                <div class="card-innr card-innr-fix">
-                    <div class="card-head d-flex justify-content-between align-items-center">
-                        <h4 class="card-title mb-0">User Details</h4>
-                        <div class="d-flex align-items-center guttar-20px">
-                            <div class="flex-col d-sm-block d-none">
-                                <a href="/#/userlist" class="btn btn-sm btn-auto btn-primary"><em class="fas fa-arrow-left mr-3"></em>Back</a>
-                            </div>
-                            <div class="flex-col d-sm-none">
-                                <a href="/#/userlist" class="btn btn-icon btn-sm btn-primary"><em class="fas fa-arrow-left"></em></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="gaps-1-5x"></div>
-                    <div class="data-details d-md-flex">
-                        <ul class="data-vr-list">
-                            <li><div class="data-state data-state-sm data-state-approved"></div> Email</li>
-                            <li><div class="data-state data-state-sm data-state-pending"></div> KYC</li>
-                            <li><div class="data-state data-state-sm data-state-approved"></div> WL</li>
-                        </ul>
-                    </div>
-                    <div class="gaps-3x"></div>
-                    <h6 class="card-sub-title">User Information</h6>
-                    <ul class="data-details-list">
-                        <li>
-                            <div class="data-details-head">Full Name</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                        <li>
-                            <div class="data-details-head">Surname</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                        <li>
-                            <div class="data-details-head">Email Address</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                        <li>
-                            <div class="data-details-head">Mobile Number</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                        <li>
-                            <div class="data-details-head">Date of Birth</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                    </ul>
-                    <div class="gaps-3x"></div>
-                    <h6 class="card-sub-title">More Information</h6>
-                    <ul class="data-details-list">
-                        <li>
-                            <div class="data-details-head">Joining Date</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                        <li>
-                            <div class="data-details-head">Reg Method</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                        <li>
-                            <div class="data-details-head">Last Login</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                        <li>
-                            <div class="data-details-head">Newsleter Subscription</div>
-                            <div class="data-details-des"></div>
-                        </li>
-                    </ul>
+      <Navbar />
+      <div className="page-content">
+        <div className="container">
+          <div className="card content-area">
+            <div className="card-innr card-innr-fix">
+              <div className="card-head d-flex justify-content-between align-items-center">
+                <h4 className="card-title mb-0">User Details</h4>
+                <div className="d-flex align-items-center guttar-20px">
+                  <div className="flex-col d-sm-block d-none">
+                    <a href="/#/userlist" className="btn btn-sm btn-auto btn-primary">
+                      <em className="fas fa-arrow-left mr-3"></em>Back
+                    </a>
+                  </div>
+                  <div className="flex-col d-sm-none">
+                    <a href="/#/userlist" className="btn btn-icon btn-sm btn-primary">
+                      <em className="fas fa-arrow-left"></em>
+                    </a>
+                  </div>
                 </div>
+              </div>
+              <div className="gaps-3x"></div>
+              <h6 className="card-sub-title">Personal Information</h6>
+              <ul className="data-details-list">
+                <li>
+                  <div className="data-details-head">Full Name</div>
+                  <div className="data-details-des">{user.full_name}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Date of Birth</div>
+                  <div className="data-details-des">{user.date_of_birth}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Nationality</div>
+                  <div className="data-details-des">{user.nationality}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Identification Number</div>
+                  <div className="data-details-des">{user.identification_number}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Phone Number</div>
+                  <div className="data-details-des">{user.phone_number}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Email Address</div>
+                  <div className="data-details-des">{user.email}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Tax Identification Number (TIN)</div>
+                  <div className="data-details-des">{user.tax_id}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Source of Funds</div>
+                  <div className="data-details-des">{user.source_of_funds}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Business License</div>
+                  <div className="data-details-des">{user.business_license}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Company Registration Certificate</div>
+                  <div className="data-details-des">{user.company_registration_certificate}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">National ID Card OR Passport (Both Sides)</div>
+                  <div className="data-details-des">{user.national_id_or_passport}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Selfie</div>
+                  <div className="data-details-des">{user.selfie}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Proof of Address (e.g., utility bill, bank statement for the business)</div>
+                  <div className="data-details-des">{user.proof_of_address}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Trade Licenses (for specific industries or activities)</div>
+                  <div className="data-details-des">{user.trade_licenses}</div>
+                </li>
+              </ul>
+              <div className="gaps-3x"></div>
+              <h6 className="card-sub-title">More Information</h6>
+              <ul className="data-details-list">
+                <li>
+                  <div className="data-details-head">Status</div>
+                  <div className="data-details-des">{user.status}</div>
+                </li>
+                <li>
+                  <div className="data-details-head">Date</div>
+                  <div className="data-details-des">{user.date}</div>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
-    </div>
-    
-    <div class="footer-bar">
-        <div class="container">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-md-8">
-                    <ul class="footer-links">
-                        <li><a href="/#/faqpage">FAQs</a></li>
-                        <li><a href="/#/policy">Privacy Policy</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4 mt-2 mt-sm-0">
-                    <div class="d-flex justify-content-between justify-content-md-end align-items-center guttar-25px pdt-0-5x pdb-0-5x">
-                        <div class="copyright-text">&copy; MUDA KYC Project.</div>
-                        <div class="lang-switch relative">
-                            {/* <a href="#" class="lang-switch-btn toggle-tigger">En <em class="ti ti-angle-up"></em></a>
-                            <div class="toggle-class dropdown-content dropdown-content-up">
-                                <ul class="lang-list">
-                                    <li><a href="#">Fr</a></li>
-                                    <li><a href="#">Bn</a></li>
-                                    <li><a href="#">Lt</a></li>
-                                </ul>
-                            </div> */}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+      </div>
 
+      <div className="footer-bar">
+        <div className="container">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-md-8">
+              <ul className="footer-links">
+                <li><a href="/#/faqpage">FAQs</a></li>
+                <li><a href="/#/policy">Privacy Policy</a></li>
+              </ul>
+            </div>
+            <div className="col-md-4 mt-2 mt-sm-0">
+              <div className="d-flex justify-content-between justify-content-md-end align-items-center guttar-25px pdt-0-5x pdb-0-5x">
+                <div className="copyright-text">&copy; MUDA KYC Project.</div>
+                <div className="lang-switch relative">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Userdetails
+export default UserDetails;
